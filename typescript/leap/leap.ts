5b568b501f5e8074a48ae1eed015f26a20b0187b
Renato Setoue <renatosetoue@gmail.com>
2026-05-18 20:47:15 -0300

    Update leap.ts

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,9 +1,2 @@
-export function isLeap(year: number) {
-	if (year % 4 === 0) {
-		if (year % 100 === 0) {
-			if (year % 400 === 0) return true;
-			return false;
-		}
-		return true;
-	} else return false;
-}
+export const isLeap = (year: number) =>
+	year % 400 === 0 ? true : year % 100 === 0 ? false : year % 4 === 0;
