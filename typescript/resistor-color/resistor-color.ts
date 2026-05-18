ceaa98af75562260eba0e8f3d0537758387194c1
Renato Setoue <renatosetoue@gmail.com>
2026-05-18 00:21:42 -0300

    resistor-color

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,28 @@
-export const colorCode = () => {
-  throw new Error('Remove this line and implement the function')
-}
+type Color =
+	| "black"
+	| "brown"
+	| "red"
+	| "orange"
+	| "yellow"
+	| "green"
+	| "blue"
+	| "violet"
+	| "grey"
+	| "white";
 
-export const COLORS = undefined
+export const COLORS = [
+	"black",
+	"brown",
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"blue",
+	"violet",
+	"grey",
+	"white",
+];
+
+export const colorCode = (color: Color): number => {
+	return COLORS.indexOf(color.toLowerCase());
+};
