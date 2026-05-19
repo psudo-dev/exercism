1998f834c4b880b45560e58a6bc81cf5efdb8496
Renato Setoue <renatosetoue@gmail.com>
2026-05-19 14:58:08 -0300

    space-age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,28 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this line and implement the function')
+type Planets =
+	| "mercury"
+	| "venus"
+	| "earth"
+	| "mars"
+	| "jupiter"
+	| "saturn"
+	| "uranus"
+	| "neptune";
+
+type EarthYears = {
+	[Key in Planets]: number;
+};
+
+const earthYears: EarthYears = {
+	mercury: 0.2408467,
+	venus: 0.61519726,
+	earth: 1.0,
+	mars: 1.8808158,
+	jupiter: 11.862615,
+	saturn: 29.447498,
+	uranus: 84.016846,
+	neptune: 164.79132,
+};
+
+export function age(planet: Planets, seconds: number): number {
+	return +(seconds / (earthYears[planet] * 31_557_600)).toFixed(2);
 }
