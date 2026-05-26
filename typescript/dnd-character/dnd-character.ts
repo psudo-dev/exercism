21aa76b565bb13659e87806ae2c68ab944952aaf
Renato Setoue <renatosetoue@gmail.com>
2026-05-25 23:30:12 -0300

    solves dnd-character exercise
    the exercise forces you to use static in the methods that already come with it and by consequences, to use the class itself instead of this when calling the static methods.

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,37 @@
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this line and implement the function')
-  }
-
-  public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this line and implement the function')
-  }
+	strength: number;
+	dexterity: number;
+	constitution: number;
+	intelligence: number;
+	wisdom: number;
+	charisma: number;
+	hitpoints: number;
+	constructor() {
+		this.strength = DnDCharacter.generateAbilityScore();
+		this.dexterity = DnDCharacter.generateAbilityScore();
+		this.constitution = DnDCharacter.generateAbilityScore();
+		this.intelligence = DnDCharacter.generateAbilityScore();
+		this.wisdom = DnDCharacter.generateAbilityScore();
+		this.charisma = DnDCharacter.generateAbilityScore();
+		this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+	}
+	public static rollDice(): number {
+		return Math.floor(Math.random() * 6) + 1;
+	}
+	public static generateAbilityScore(): number {
+		const dicesArray = [
+			DnDCharacter.rollDice(),
+			DnDCharacter.rollDice(),
+			DnDCharacter.rollDice(),
+			DnDCharacter.rollDice(),
+		];
+		dicesArray.sort((a, b) => b - a).pop();
+		const sum = [...dicesArray].reduce((acc, num) => {
+			return acc + num;
+		}, 0);
+		return sum;
+	}
+	public static getModifierFor(abilityValue: number): number {
+		return Math.floor((abilityValue - 10) / 2);
+	}
 }
